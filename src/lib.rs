428257fd064e7693c1697d4f621351dbfcada400
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-01-03 22:24:59 +0100

    Change to a pattern based approach

--- src/lib.rs
+++ src/lib.rs
@@ -33,51 +33,43 @@ impl<SPI, E> Ws2812<SPI>
 where
     SPI: FullDuplex<u8, Error = E>,
 {
-    /// The SPI bus should run with 3 Mhz, otherwise this won't work.
+    /// The SPI bus should run exactly with the provided frequency
     ///
     /// You may need to look at the datasheet and your own hal to verify this.
     ///
-    /// Please ensure that the mcu is pretty fast, otherwise weird timing
-    /// issues will occur
+    /// Please ensure that the mcu is pretty fast (around 48 MHz worked for me),
+    /// otherwise weird timing issues will occur
     pub fn new(spi: SPI, timing: Timing) -> Ws2812<SPI> {
         Self { spi, timing }
-    }
-
-    /// Stash bits in `byte`, until it's full, then write it out
-    fn stash_byte(&mut self, bit: bool, byte: &mut u8, count: &mut u8) -> Result<(), E> {
-        *byte = (*byte << 1) | bit as u8;
-        *count += 1;
-        if *count == 8 {
-            self.spi.read().ok();
-            block!(self.spi.send(*byte))?;
-            *count = 0;
-        }
-        Ok(())
     }
 
     /// Write a single byte for ws2812 devices
     fn write_color(&mut self, data: Color) -> Result<(), E> {
         let mut serial_bits = (data.g as u32) << 16 | (data.r as u32) << 8 | (data.b as u32) << 0;
-        let mut serial_data = 0;
+        let mut serial_data: u32 = 0;
         let mut serial_count = 0;
         for _ in 0..24 {
-            let one_bits = if (serial_bits & 0x00800000) != 0 {
-                self.timing.one_high
+            let pattern = if (serial_bits & 0x00800000) != 0 {
+                self.timing.one_pattern
             } else {
-                self.timing.zero_high
+                self.timing.zero_pattern
             };
-            let zero_bits = self.timing.total - one_bits;
-            for _ in 0..one_bits {
-                self.stash_byte(true, &mut serial_data, &mut serial_count)?;
-            }
-            for _ in 0..zero_bits {
-                self.stash_byte(false, &mut serial_data, &mut serial_count)?;
+            serial_count += self.timing.len;
+            serial_data |= pattern << (32 - serial_count);
+            while serial_count > 7 {
+                let data = (serial_data >> 24) as u8;
+                self.spi.read().ok();
+                block!(self.spi.send(data))?;
+                serial_data <<= 8;
+                serial_count -= 8;
             }
             serial_bits <<= 1;
         }
-        // Now fill the last byte up
-        while serial_count != 0 {
-            self.stash_byte(false, &mut serial_data, &mut serial_count)?;
+        if serial_count != 0 {
+            serial_data <<= 8 - serial_count;
+            let data = (serial_data >> 24) as u8;
+            self.spi.read().ok();
+            block!(self.spi.send(data))?;
         }
         Ok(())
     }
@@ -96,7 +88,7 @@ where
         for item in iterator {
             self.write_color(item)?;
         }
-        for _ in 0..(self.timing.flush) / 8 + 1 {
+        for _ in 0..(self.timing.flush_bytes) {
             block!(self.spi.send(0))?;
             self.spi.read().ok();
         }
@@ -104,15 +96,16 @@ where
     }
 }
 
+#[derive(Debug)]
 pub struct Timing {
-    one_high: usize,
-    zero_high: usize,
-    total: usize,
-    total_max: usize,
-    flush: usize,
+    one_pattern: u32,
+    zero_pattern: u32,
+    len: u8,
+    flush_bytes: usize,
 }
 
 impl Timing {
+    /// Create timing values for the provided frequency
     pub fn new(mhz: u32) -> Option<Self> {
         if mhz < 2_000_000 {
             return None;
@@ -120,30 +113,47 @@ impl Timing {
         static ONE_HIGH: u32 = 1_510_000;
         static ZERO_HIGH: u32 = 5_000_000;
         static TOTAL: u32 = 1_100_000;
-        static TOTAL_MAX: u32 = 200_000;
         static FLUSH: u32 = 3_000;
 
-        let mut zero_high = (mhz / ZERO_HIGH) as usize;
+        let mut zero_high = mhz / ZERO_HIGH;
         // Make sure we have at least something
         if zero_high == 0 {
             zero_high = 1;
         }
 
         // Round up
-        let one_high = (mhz / ONE_HIGH + 1) as usize;
-        let mut total = (mhz / TOTAL + 1) as usize;
+        let one_high = mhz / ONE_HIGH + 1;
+        let mut total = mhz / TOTAL + 1;
         // Make sure total is at least one higher than one_high
         if total == one_high {
             total = one_high + 1;
         }
-        let total_max = (mhz / TOTAL_MAX + 1) as usize;
-        let flush = (mhz / FLUSH + 1) as usize;
+        if total > 28 {
+            return None;
+        }
+        let flush = ((mhz / FLUSH + 1) / 8 + 1) as usize;
+        // Create patterns
+        let mut one_pattern = 0;
+        let mut zero_pattern = 0;
+        for _ in 0..one_high {
+            one_pattern <<= 1;
+            one_pattern |= 1;
+        }
+        for _ in 0..total - one_high {
+            one_pattern <<= 1;
+        }
+        for _ in 0..zero_high {
+            zero_pattern <<= 1;
+            zero_pattern |= 1;
+        }
+        for _ in 0..total - zero_high {
+            zero_pattern <<= 1;
+        }
         Some(Self {
-            one_high,
-            zero_high,
-            total,
-            total_max,
-            flush,
+            one_pattern,
+            zero_pattern,
+            len: total as u8,
+            flush_bytes: flush,
         })
     }
 }
